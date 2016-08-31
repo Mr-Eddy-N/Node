45c5d5eb46b648c9886eebce70ea9bba055517fa
Edgar <edgarreyesrojas@gmail.com>
2016-08-31 16:03:23 -0500

    datos

--- bot_saves_princess/bot_saves_princess.js
+++ bot_saves_princess/bot_saves_princess.js
@@ -1,6 +1,67 @@
 function displayPathtoPrincess(dimension, grid)
 {
-    // Your Code here
+   // console.log("display path")
+   Moves=[];
+   Matrix=[[],[],[]];
+   
+  Matrix=gidToMatrix(dimension,grid); 
+    isSolution=false;
+    var ini= new Nodo();
+    ini.ID=0;
+    ini.Move="initial";
+    ini.Parent=null;
+    Moves.push(ini);
+    var count=0;
+    //console.log("Matrix: "+Matrix.length)Matrix.forEach(function(a){console.log(a)})
+    while(!isSolution){
+     
+        
+        
+    }
+    
 }
-
+function expande(){
+    
+}
+function isPisibleMoveIt(M,move){
+    switch(move){
+        case "D":
+            break;
+        case "U":
+            break
+        case "L":
+        break;
+        case "R":
+        break:
+            
+    }
+    
+}
+function get_M(grid){
+    grid.forEach(function(){
+        if(p=="m")
+            response = p;
+    });
+}
+function gidToMatrix(d,grid){
+    console.log("gridToMatrix")
+ var count =0;
+    response=[];
+    for (var r = 0;r<d;r++){
+        var _r=[];
+        for (var c = 0;c<d;c++){
+           _r[c]=grid[count];
+               count++;
+        }
+        response.push(_r);
+    }
+return response;
+}
+Nodo=function(){
+    this.Move="";
+    this.ID="";
+    this.Parent=null;
+}
 console.log("bot save princess");
+grid=["-","-","-","-","m","-","p","-","-"];
+displayPathtoPrincess(3,grid);
